cbe15ec6b5c2556192cc95f5f40fad7f87f81095
Mihail (Mike) <janev94@users.noreply.github.com>
2022-06-13 21:04:17 +0300

    Added GPLv2 header

--- tcp_newcwv.c
+++ tcp_newcwv.c
@@ -1,6 +1,19 @@
 /*
- * New-CWV implementation for Linux: draft-ietf-tcpm-newcwv-05.txt
- */
+* Linux 5.4.x port for the New-CWV implementation for Linux: draft-ietf-tcpm-newcwv-05.txt
+* Original New CWV implementation provided by Ziaul Hossain and Raffaello Secchi
+*
+* --------------------------------------------------------------------------------
+*
+* Copyright (C) 2022 Mihail Yanev
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+* 
+* ---------------------------------------------------------------------
+* New-CWV implementation for Linux: draft-ietf-tcpm-newcwv-05.txt
+*/
 
 #include <linux/module.h>
 #include <net/tcp.h>
